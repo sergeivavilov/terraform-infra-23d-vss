18534e5dd6e92f5a50c802f2fc45f04386b4c9a5
sergeivavilov <157989606+sergeivavilov@users.noreply.github.com>
2024-05-06 18:42:00 -0600

    Update eks.tf

--- eks-module/eks.tf
+++ eks-module/eks.tf
@@ -1,15 +1,15 @@
 resource "aws_eks_cluster" "cluster" {
-  name     = var.name
+  name     = var.eks_name
   role_arn = aws_iam_role.eks_cluster_role.arn
-  version  = "1.29"
+  version  = var.eks_version
 
   vpc_config {
-    subnet_ids = var.subnet_ids
+    subnet_ids = var.eks_vpc_subnet_ids
     security_group_ids = [aws_security_group.eks_cluster_sg.id]
   }
 
   kubernetes_network_config {
-    service_ipv4_cidr = var.CIDR
+    service_ipv4_cidr = var.k8_net_config_cidr
   }
 
   # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
@@ -19,74 +19,63 @@ resource "aws_eks_cluster" "cluster" {
   ]
 
   tags = {
-    Name = var.cluster_tag
+    Name = var.tag_name
   }
 }
 
 # trust policy for the role
 data "aws_iam_policy_document" "assume_role" {
   statement {
-    effect = "Allow"
+    effect = var.iam_pol_effect
 
     principals {
-      type        = "Service"
-      identifiers = ["eks.amazonaws.com"]
+      type        = var.iam_pol_prin_type
+      identifiers = var.iam_pol_prin_identifiers
     }
 
-    actions = ["sts:AssumeRole"]
+    actions = var.iam_pol_actions
   }
 }
 
 # create IAM role
 resource "aws_iam_role" "eks_cluster_role" {
-  name               = "${var.name}-eks-iam-role"
+  name               = var.iam_role_name
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
-# attach policy to the role, policy managed by AWS
+# attach policy to the role
 resource "aws_iam_role_policy_attachment" "eks_cluster_role-AmazonEKSClusterPolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  policy_arn = var.iam_role_policy_arn
   role       = aws_iam_role.eks_cluster_role.name
 }
 
 
 resource "aws_security_group" "eks_cluster_sg" {
-  name        = "EKS Cluster ${var.name} Security Group"
-  description = "Allow All inbound traffic from Self and all outbound traffic"
-  vpc_id      = var.vpc_id
+  name        = var.eks_sg_name
+  description = var.eks_sg_description
+  vpc_id      = var.eks_sg_vpc_id
 
-  tags = {
-    Name = "${var.name}-sg"
-    "kubernetes.io/cluster/project-x-dev" = "owned"
-    "aws:eks:cluster-name"	= var.name
-  }
+  tags = var.eks_sg_tag
 }
 
-# resource "aws_vpc_security_group_ingress_rule" "allow_worker_traffic" {
-#   security_group_id = aws_security_group.eks_cluster_sg.id
-#   referenced_security_group_id         = aws_security_group.eks_worker_sg.id
-#   # from_port         = 0
-#   ip_protocol       = "-1"
-#   # to_port           = 0
-# }
 resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
   security_group_id = aws_security_group.eks_cluster_sg.id
   referenced_security_group_id         = aws_security_group.eks_cluster_sg.id
-  # from_port         = 0
-  ip_protocol       = "-1"
-  # to_port           = 0
+  from_port         = var.sg_allow_tls_port
+  ip_protocol       = "tcp"
+  to_port           = var.sg_allow_tls_port
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   security_group_id = aws_security_group.eks_cluster_sg.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1" # semantically equivalent to all ports
+  cidr_ipv4         = var.sg_all_ipv4_traffic
+  ip_protocol       = var.sg_all_port_protocol
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv6" {
   security_group_id = aws_security_group.eks_cluster_sg.id
-  cidr_ipv6         = "::/0"
-  ip_protocol       = "-1" # semantically equivalent to all ports
+  cidr_ipv6         = var.sg_all_ipv6_traffic
+  ip_protocol       = var.sg_all_port_protocol
 }
 
 # 
