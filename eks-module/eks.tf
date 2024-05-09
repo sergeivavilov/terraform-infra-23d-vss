c42ef5d430ec8e027b2814562185b2f7f60fd7af
sergeivavilov <157989606+sergeivavilov@users.noreply.github.com>
2024-05-08 23:21:05 -0600

    Update eks.tf

--- eks-module/eks.tf
+++ eks-module/eks.tf
@@ -86,3 +86,7 @@ output "endpoint" {
 output "kubeconfig-certificate-authority-data" {
   value = aws_eks_cluster.cluster.certificate_authority[0].data
 }
+
+output "worker_security_group_id" {
+  value = aws_security_group.eks_worker_sg.id
+}
