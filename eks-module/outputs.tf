cc33bcb2dac1b4f63c21bdc6fd0f062301a9bfb6
sergeivavilov <157989606+sergeivavilov@users.noreply.github.com>
2024-05-09 22:48:13 -0600

    Update outputs.tf

--- eks-module/outputs.tf
+++ eks-module/outputs.tf
@@ -1,6 +1,6 @@
-output "worker_security_group_id" {
-  value = aws_security_group.eks_worker_sg.id
-}
+# output "worker_security_group_id" {
+#   value = aws_security_group.eks_worker_sg.id
+# }
 
 output "worker_role_arn" {
   value = aws_iam_role.eks_worker_role.arn
