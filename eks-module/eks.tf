479c14f60afef51ecea439d459f5fc3a60b71e4c
sergeivavilov <157989606+sergeivavilov@users.noreply.github.com>
2024-05-13 21:52:46 -0600

    Update eks.tf

--- eks-module/eks.tf
+++ eks-module/eks.tf
@@ -1,124 +1,143 @@
+# Define the AWS EKS cluster with specific configurations.
 resource "aws_eks_cluster" "cluster" {
-  name     = var.eks_name
-  role_arn = aws_iam_role.eks_cluster_role.arn
-  version  = var.eks_version
+  name     = var.eks_name  # Name of the EKS cluster.
+  role_arn = aws_iam_role.eks_cluster_role.arn  # ARN of the IAM role for the EKS cluster.
+  version  = var.eks_version  # Version of Kubernetes to use for the EKS cluster.
 
+  # Configuration for the VPC settings of the EKS cluster.
   vpc_config {
-    subnet_ids         = var.eks_vpc_subnet_ids
-    security_group_ids = [aws_security_group.eks_cluster_sg.id]
+    subnet_ids         = var.eks_vpc_subnet_ids  # List of subnet IDs for the EKS cluster.
+    security_group_ids = [aws_security_group.eks_cluster_sg.id]  # List of security group IDs attached to the EKS cluster.
   }
 
+  # Configuration for the Kubernetes network settings within the EKS cluster.
   kubernetes_network_config {
-    service_ipv4_cidr = var.k8_net_config_cidr
+    service_ipv4_cidr = var.k8_net_config_cidr  # CIDR block for Kubernetes service IP addresses.
   }
 
-  # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
-  # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
+  # Ensures that the specified IAM role permissions are correctly set up before creating the EKS cluster.
   depends_on = [
     aws_iam_role_policy_attachment.eks_cluster_role-AmazonEKSClusterPolicy
   ]
 
+  # Tags to assign to the EKS cluster for identification and management.
   tags = {
-    Name = var.tag_name
+    Name = var.tag_name  # Tag with the name of the EKS cluster.
   }
 }
 
-# trust policy for the role
+# Define the trust policy for the EKS cluster IAM role.
 data "aws_iam_policy_document" "assume_role" {
+  # Statement block within the policy document.
   statement {
-    effect = var.iam_pol_effect
+    effect = var.iam_pol_effect  # Effect of the statement, typically 'Allow'.
 
+    # Defines the principal that can assume this role.
     principals {
-      type        = var.iam_pol_prin_type
-      identifiers = var.iam_pol_prin_identifiers
+      type        = var.iam_pol_prin_type  # Type of principal, such as 'Service'.
+      identifiers = var.iam_pol_prin_identifiers  # Identifiers of the principal, e.g., 'eks.amazonaws.com'.
     }
 
-    actions = var.iam_pol_actions
+    actions = var.iam_pol_actions  # Actions allowed by the policy, typically 'sts:AssumeRole'.
   }
 }
 
-# create IAM role
+# Create the IAM role for the EKS cluster.
 resource "aws_iam_role" "eks_cluster_role" {
-  name               = var.iam_role_name
-  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+  name               = var.iam_role_name  # Name of the IAM role.
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json  # JSON version of the policy document.
+
+  tags = {
+    Name = "eks-cluster-role"  # Tag attached to the IAM role.
+  }
 }
 
-# attach policy to the role
+# Attach a policy to the IAM role for the EKS cluster.
 resource "aws_iam_role_policy_attachment" "eks_cluster_role-AmazonEKSClusterPolicy" {
-  policy_arn = var.iam_role_policy_arn
-  role       = aws_iam_role.eks_cluster_role.name
+  role       = aws_iam_role.eks_cluster_role.name  # Reference to the IAM role.
+  policy_arn = var.iam_role_policy_arn  # ARN of the policy to attach.
 }
 
+# Define a security group for the EKS cluster.
 resource "aws_security_group" "eks_cluster_sg" {
-  name        = var.eks_sg_name
-  description = var.eks_sg_description
-  vpc_id      = var.eks_sg_vpc_id
+  name        = var.eks_sg_name  # Name of the security group.
+  description = var.eks_sg_description  # Description of the security group.
+  vpc_id      = var.eks_sg_vpc_id  # VPC ID where the security group is created.
 
-  tags = var.eks_sg_tag
+  tags = var.eks_sg_tag  # Tags attached to the security group.
 }
 
+# Define an ingress rule for the EKS cluster security group to allow TLS traffic.
 resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
-  security_group_id             = aws_security_group.eks_cluster_sg.id
-  referenced_security_group_id  = aws_security_group.eks_cluster_sg.id
-  from_port                     = var.sg_allow_tls_port
-  ip_protocol                   = "tcp"
-  to_port                       = var.sg_allow_tls_port
+  security_group_id             = aws_security_group.eks_cluster_sg.id  # ID of the security group.
+  referenced_security_group_id  = aws_security_group.eks_cluster_sg.id  # ID of the security group to reference.
+  from_port                     = var.sg_allow_tls_port  # Starting port range (TLS port).
+  ip_protocol                   = "tcp"  # Protocol used.
+  to_port                       = var.sg_allow_tls_port  # Ending port range (TLS port).
 }
 
+# Define an egress rule for the EKS cluster security group to allow all outbound IPv4 traffic.
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
-  security_group_id = aws_security_group.eks_cluster_sg.id
-  cidr_ipv4         = var.sg_all_ipv4_traffic
-  ip_protocol       = var.sg_all_port_protocol
+  security_group_id = aws_security_group.eks_cluster_sg.id  # ID of the security group.
+  cidr_ipv4         = var.sg_all_ipv4_traffic  # CIDR block for outbound traffic.
+  ip_protocol       = var.sg_all_port_protocol  # Protocol used, typically '-1' for all.
 }
 
+# Define an egress rule for the EKS cluster security group to allow all outbound IPv6 traffic.
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv6" {
-  security_group_id = aws_security_group.eks_cluster_sg.id
-  cidr_ipv6         = var.sg_all_ipv6_traffic
-  ip_protocol       = var.sg_all_port_protocol
+  security_group_id = aws_security_group.eks_cluster_sg.id  # ID of the security group.
+  cidr_ipv6         = var.sg_all_ipv6_traffic  # CIDR block for outbound traffic.
+  ip_protocol       = var.sg_all_port_protocol  # Protocol used, typically '-1' for all.
 }
 
-# Define the eks_worker_sg resource
+# Define a security group for EKS worker nodes.
 resource "aws_security_group" "eks_worker_sg" {
-  name        = "eks-worker-sg"
-  description = "Security group for EKS worker nodes"
-  vpc_id      = var.eks_sg_vpc_id
+  name        = "eks-worker-sg"  # Name of the security group.
+  description = "Security group for EKS worker nodes"  # Description of the security group.
+  vpc_id      = var.eks_sg_vpc_id  # VPC ID where the security group is created.
 
+  # Ingress rule to allow SSH access.
   ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 22  # SSH port.
+    to_port     = 22  # SSH port.
+    protocol    = "tcp"  # Protocol used.
+    cidr_blocks = ["0.0.0.0/0"]  # CIDR block allowing access from any IP.
   }
 
+  # Ingress rule to allow HTTPS access.
   ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 443  # HTTPS port.
+    to_port     = 443  # HTTPS port.
+    protocol    = "tcp"  # Protocol used.
+    cidr_blocks = ["0.0.0.0/0"]  # CIDR block allowing access from any IP.
   }
 
+  # Egress rule to allow all outbound traffic.
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 0  # All ports.
+    to_port     = 0  # All ports.
+    protocol    = "-1"  # All protocols.
+    cidr_blocks = ["0.0.0.0/0"]  # CIDR block allowing access to any IP.
   }
 
   tags = {
-    Name = "eks-worker-sg"
+    Name = "eks-worker-sg"  # Tag attached to the security group.
   }
 }
 
+# Output the endpoint for the EKS cluster.
 output "endpoint" {
-  value = aws_eks_cluster.cluster.endpoint
+  value = aws_eks_cluster.cluster.endpoint  # Endpoint of the EKS cluster.
 }
 
+# Output the certificate authority data for the EKS cluster.
 output "kubeconfig-certificate-authority-data" {
-  value = aws_eks_cluster.cluster.certificate_authority[0].data
+  value = aws_eks_cluster.cluster.certificate_authority[0].data  # Certificate authority data.
 }
 
+# Output the ID of the security group for the EKS worker nodes.
 output "worker_security_group_id" {
-  value = [aws_security_group.eks_worker_sg.id]
+  value = [aws_security_group.eks_worker_sg.id]  # ID of the security group.
 }
 
 # The following resources were commented out:
