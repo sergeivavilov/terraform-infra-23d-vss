3e3225175de58b2c14e79d8a2d9a4f41a2a0d90b
sergeivavilov <157989606+sergeivavilov@users.noreply.github.com>
2024-05-13 22:09:30 -0600

    Update outputs.tf

--- eks-module/outputs.tf
+++ eks-module/outputs.tf
@@ -1,7 +1,9 @@
+# This is a commented-out output for the security group ID of EKS worker nodes.
 # output "worker_security_group_id" {
-#   value = aws_security_group.eks_worker_sg.id
+#   value = aws_security_group.eks_worker_sg.id  # Retrieves the ID of the EKS worker security group.
 # }
 
+# Output the ARN of the IAM role for EKS worker nodes.
 output "worker_role_arn" {
-  value = aws_iam_role.eks_worker_role.arn
+  value = aws_iam_role.eks_worker_role.arn  # Retrieves the ARN of the IAM role assigned to EKS worker nodes.
 }
