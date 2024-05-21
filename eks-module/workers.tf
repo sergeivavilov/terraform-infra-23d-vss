df7f6fb33cab9bb998a5bdf101d8c749e63a254e
sergeivavilov <157989606+sergeivavilov@users.noreply.github.com>
2024-05-20 19:34:30 -0600

    Update workers.tf

--- eks-module/workers.tf
+++ eks-module/workers.tf
@@ -1,83 +1,45 @@
-# locals {
-#   version = "1.29"
-# }
-# data "aws_ssm_parameter" "eks_ami_id" {
-#   name = "/aws/service/eks/optimized-ami/${local.version}/amazon-linux-2/recommended/image_id"
-# }
 
-# resource "aws_launch_template" "eks_workers" {
-#   name_prefix   = var.eks_workers_lt_name
-#   image_id      = data.aws_ssm_parameter.eks_ami_id.value
-#   instance_type = var.eks_workers_lt_instance_type
+data "aws_ssm_parameter" "eks_ami_id" {
+  name = "/aws/service/eks/optimized-ami/1.29/amazon-linux-2/recommended/image_id"
+}
 
-#     # #!/bin/bash
-#     # set -o xtrace
-#     # /etc/eks/bootstrap.sh project-x-dev
+resource "aws_launch_template" "eks_workers" {
+  name_prefix   = "${var.name}-eks-worker-nodes"
+  image_id      = data.aws_ssm_parameter.eks_ami_id.value
+  instance_type = var.instance_type
+  depends_on = [null_resource.update_aws_auth,# kubernetes_config_map.aws_auth,
+    aws_iam_role_policy_attachment.example-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.example-AmazonEKS_CNI_Policy,
+  aws_iam_role_policy_attachment.example-AmazonEC2ContainerRegistryReadOnly]
 
-# # iam::aws:policy/AmazonEKSWorkerNodePolicy"
-# # iam::aws:policy/AmazonEKS_CNI_Policy"
-# # iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  vpc_security_group_ids = [aws_security_group.eks_worker_sg.id]
+  user_data = base64encode(<<-EOF
+#!/bin/bash
+set -o xtrace
+/etc/eks/bootstrap.sh ${var.name}
+EOF
+  )
+}
 
-# }
+resource "aws_security_group" "eks_worker_sg" {
+  name        = "eks-worker-sg"
+  description = "Security group for EKS worker nodes"
 
-# resource "aws_autoscaling_group" "eks_workers" {
-#   capacity_rebalance  = var.capacity_rebalance
-#   desired_capacity    = var.desired_capacity
-#   max_size            = var.max_size
-#   min_size            = var.min_size
-#   vpc_zone_identifier = var.vpc_zone_identifier
+  vpc_id = var.vpc_id
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-#   mixed_instances_policy {
-#     instances_distribution {
-#       on_demand_base_capacity                  = var.on_demand_base_capacity
-#       on_demand_percentage_above_base_capacity = var.on_demand_percentage_above_base_capacity
-#       spot_allocation_strategy                 = var.spot_allocation_strategy
-#     }
-
-#     launch_template {
-#       launch_template_specification {
-#         launch_template_id = aws_launch_template.eks_workers.id
-#       }
-
-#       override {
-#         instance_type     = var.lt_override_instance_type_1
-#         weighted_capacity = var.lt_override_weighted_capacity_1
-#       }
-
-#       override {
-#         instance_type     = var.lt_override_instance_type_2
-#         weighted_capacity = var.lt_override_weighted_capacity_2
-#       }
-#     }
-#   }
-# }
-
-# resource "aws_iam_role" "example" {
-#   name = "eks-node-group-example"
-
-#   assume_role_policy = jsonencode({
-#     Statement = [{
-#       Action = "sts:AssumeRole"
-#       Effect = "Allow"
-#       Principal = {
-#         Service = "ec2.amazonaws.com"
-#       }
-#     }]
-#     Version = "2012-10-17"
-#   })
-# }
-
-# resource "aws_iam_role_policy_attachment" "example-AmazonEKSWorkerNodePolicy" {
-#   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-#   role       = aws_iam_role.example.name
-# }
-
-# resource "aws_iam_role_policy_attachment" "example-AmazonEKS_CNI_Policy" {
-#   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-#   role       = aws_iam_role.example.name
-# }
-
-# resource "aws_iam_role_policy_attachment" "example-AmazonEC2ContainerRegistryReadOnly" {
-#   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-#   role       = aws_iam_role.example.name
-# }
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+output "worker_sg" {
+  value = aws_security_group.eks_worker_sg
+}
