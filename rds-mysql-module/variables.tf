2a43e0b9406b343469b51761a046cde762604c25
sergeivavilov <157989606+sergeivavilov@users.noreply.github.com>
2024-05-20 19:34:23 -0600

    Create variables.tf

---
+++ rds-mysql-module/variables.tf
@@ -0,0 +1,36 @@
+variable "db_instance_identifier" {
+
+}
+variable "db_allocated_storage" {
+
+}
+variable "db_name" {
+
+}
+variable "engine" {
+
+}
+variable "engine_version" {
+
+}
+variable "instance_class" {
+
+}
+variable "db_username" {
+
+}
+variable "backup_retention_period" {
+
+}
+variable "vpc_id" {
+
+}
+variable "db_subnet_group_name" {
+
+}
+variable "subnet_ids" {
+
+}
+variable "worker_sg" {
+
+}
