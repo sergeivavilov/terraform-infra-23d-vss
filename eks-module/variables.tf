a01520b0435abd67a0ac7ee5bbe4b5eb4b0ea8be
sergeivavilov <157989606+sergeivavilov@users.noreply.github.com>
2024-05-20 19:34:35 -0600

    Create variables.tf

---
+++ eks-module/variables.tf
@@ -0,0 +1,36 @@
+variable "name" {
+
+}
+variable "CIDR" {
+
+}
+variable "instance_type" {
+
+}
+variable "cluster_tag" {
+
+}
+variable "vpc_id" {
+
+}
+variable "subnet_ids" {
+
+}
+variable "alternative_instance_type" {
+
+}
+variable "capacity" {
+
+}
+variable "spot_allocation_strategy" {
+
+}
+variable "availability-zone" {
+
+}
+variable "subnet_ip_range_1" {
+
+}
+variable "subnet_ip_range_2" {
+
+}
